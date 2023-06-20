f9b661586eb37a1e0734ee0079e80d22b9246722
Daniil Shliakhov <shliakhovdan@gmail.com>
2023-06-20 20:06:29 +0300

    Added test authorization

--- blogify-backend/index.js
+++ blogify-backend/index.js
@@ -2,8 +2,17 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
     response.send('Hello, Node.js server!');
+});
+
+app.post('/auth/login', (request, response) => {
+    console.log(request.body);
+    response.json({
+        success: true,
+    });
 });
 
 app.listen(4444, (error) => {
